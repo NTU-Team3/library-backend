e56c759ae514f8e12cca76ef8e4cfc62322c10d5
swaichee <98152745+swaichee@users.noreply.github.com>
2022-06-28 16:06:57 +0800

    added db connection

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-const router = require("./routes/");
+const express = require("express");
+//const router = require("./routes/");
 const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+const path = require("path");
+//const mongoose = require("mongoose");
+const connectDB = require("./server/database/connection");
 
-dotenv.config();
+const app = express();
 
-const username = encodeURIComponent("royston");
-const password = encodeURIComponent(process.env.MongoPW);
+//dotenv.config();
 
+/*const username = encodeURIComponent("royston");
+const password = encodeURIComponent(process.env.MongoPW);*/
+
+dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 3000;
-const uri = `mongodb+srv://${username}:${password}@libray.ikse0.mongodb.net/?retryWrites=true&w=majority`;
+
+// mongodb connection
+connectDB();
+
+/*const uri = `mongodb+srv://${username}:${password}@libray.ikse0.mongodb.net/?retryWrites=true&w=majority`;
+
+"mongodb+srv://Admin:Gr0up3@cluster0.wtyju.mongodb.net/Library?retryWrites=true&w=majority"
 
 mongoose
   .connect(uri)
@@ -18,3 +30,7 @@ mongoose
     });
   })
   .catch((err) => console.log(err));
+*/
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
