5cce417d3eb29ff1a2ce84f960d729c83d869126
swaichee <98152745+swaichee@users.noreply.github.com>
2022-07-07 11:52:04 +0800

    add allbooks and bookdetail endpoints

--- server/controller/public.controller.js
+++ server/controller/public.controller.js
@@ -1,4 +1,5 @@
 const publicService = require("../services/public.service");
+const ObjectId = require("mongodb").ObjectID;
 
 /**
  * Method: GET /public/newreleases
@@ -6,6 +7,11 @@ const publicService = require("../services/public.service");
  */
 
 class PublicController {
+  async allbooks(req, res) {
+    const { status, data, message } = await publicService.allbooks();
+    res.status(status);
+    res.json({ message, data });
+  }
   async newreleases(req, res) {
     const { status, data, message } = await publicService.newreleases();
     res.status(status);
@@ -14,6 +20,19 @@ class PublicController {
 
   async toprated(req, res) {
     const { status, data, message } = await publicService.toprated();
+    res.status(status);
+    res.json({ message, data });
+  }
+
+  async bookdetail(req, res) {
+    const bookId = req.params.bookid;
+    if (!ObjectId.isValid(bookId)) {
+      res.status(400);
+      return res.send(
+        `Controller (book detail) - Invalid book id, typeof objectId expected.`
+      );
+    }
+    const { status, data, message } = await publicService.bookdetail(bookId);
     res.status(status);
     res.json({ message, data });
   }
