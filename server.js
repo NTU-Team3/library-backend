649537aeaaa6740cf4a6b674aa6cc6e0fdbb823c
swaichee <98152745+swaichee@users.noreply.github.com>
2022-07-08 14:31:40 +0800

    test

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 router.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
