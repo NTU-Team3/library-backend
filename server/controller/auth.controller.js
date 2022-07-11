e408d678c633aef701d746bf03244faf23a8d160
swaichee <98152745+swaichee@users.noreply.github.com>
2022-07-11 20:32:56 +0800

    Add register and login endpoints

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,13 +1,64 @@
 const authService = require("../services/auth.service");
 
 class AuthController {
-  async login(req, res, next) {
-    console.log("Password received backend");
-    console.log(req.body);
+  /** =======================================================================================
+   *  Controller logic for registering new members
+   *  @param: username - user display name
+   *  @param: email - email address
+   *  @param: password - plaintext password
+   *
+   ** ======================================================================================= */
+  async register(req, res) {
+    const { username, email, plaintextPassword } = req.body;
 
+    /* validation */
+    if (!username || typeof username !== "string") {
+      return res.status(400).json({
+        message: "Username cannot be empty and must be in text format",
+      });
+    }
+
+    if (!email || typeof email !== "string") {
+      return res
+        .status(400)
+        .json({ message: "Email cannot be empty and must be in text format" });
+    }
+
+    if (!plaintextPassword || typeof plaintextPassword !== "string") {
+      return res.status(400).json({
+        message: "Password cannot be empty and must be in text format.",
+      });
+    }
+
+    if (plaintextPassword.length < 5) {
+      return res.status(400).json({
+        message: "Password should be at least 5 characters long",
+      });
+    }
+
+    const { status, data, message } = await authService.register(
+      username,
+      email,
+      plaintextPassword
+    );
+    res.status(status);
+    res.json({ message, data });
+    return res;
+  }
+
+  async login(req, res) {
     const { email, password } = req.body;
     console.log(`${email} ${password} `);
-    next();
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "email or password cannot be empty.",
+      });
+    }
+    const { status, data, message } = await authService.login(email, password);
+    console.log(`status ${status}, message: ${message}, data: ${data}`);
+    res.status(status);
+    res.json({ message, data });
   }
 }
 
